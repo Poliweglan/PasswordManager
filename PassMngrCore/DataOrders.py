a691fcacf4819b2eec465bf3031e5e08b31a06d2
Jakub Jozwik <kubojadpl@gmail.com>
2022-06-14 00:33:43 +0200

    create with statement for database sqlite

---
+++ PassMngrCore/DataOrders.py
@@ -0,0 +1,24 @@
+import sqlite3
+from sqlite3 import Error
+
+
+class BaseConnect:
+    def __init__(self, file_db):
+        self.file_db = file_db
+        self.conn = None
+
+    def __enter__(self):
+        try:
+            print(f"connect to database {self.file_db}")
+            self.conn = sqlite3.connect(self.file_db)
+        except Error as e:
+            print(e)
+
+        print(self.conn, self.file_db)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
+        self.conn = None
+        print(f"close connect with database {self.file_db}")
+        print(self.conn, self.file_db)
